4bc536e4725dfb2a79ca1f2f66e0232a57e891d6
nikitagowardhan <nikitagowar02@gmail.com>
2024-04-12 18:59:23 +0530

    Spring After Advicer

---
+++ src/model/Student.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Student {
+	
+	
+	public void a()
+	{
+		System.out.println(" This is method for actual business logic");
+	}
+
+}
---
+++ src/pack1/AfterAdvice.java
@@ -0,0 +1,19 @@
+package pack1;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+public class AfterAdvice implements AfterReturningAdvice {
+
+	@Override
+	public void afterReturning(Object arg0, Method arg1, Object[] arg2, Object arg3) throws Throwable {
+		// TODO Auto-generated method stub
+		
+		 System.out.println("additional concern after returning advice");  
+	}
+	 
+	
+	
+
+}
